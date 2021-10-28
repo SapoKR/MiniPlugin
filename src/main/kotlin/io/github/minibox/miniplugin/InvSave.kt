3ced4e69496bc6638190324802c53ab20e1986da
minibox24 <minibox724@gmail.com>
2021-10-28 10:58:21 +0900

    반인벤세이브

--- src/main/kotlin/io/github/minibox/miniplugin/InvSave.kt
+++ src/main/kotlin/io/github/minibox/miniplugin/InvSave.kt
@@ -1,5 +1,27 @@
 package io.github.minibox.miniplugin
 
+import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.inventory.ItemStack
 
-class InvSave : Listener {}
+class InvSave : Listener {
+    @EventHandler
+    fun dropHead(event: PlayerDeathEvent) {
+        var dropCount = 0
+
+        event.drops.shuffle()
+        val drops = event.drops as MutableList
+
+        val iterator = drops.iterator()
+        while (iterator.hasNext()) {
+            val item: ItemStack = iterator.next()
+
+            if (dropCount >= drops.size / 2) break
+
+            iterator.remove()
+            event.itemsToKeep.add(item)
+            dropCount += 1
+        }
+    }
+}
